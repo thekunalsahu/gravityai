28963cf7ff447ba9090d4495f1c6f98ea16228e9
thekunalsahu <mr.kunals369@gmail.com>
2026-05-05 11:15:45 +0530

    Update main.dart url Update

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ import 'package:file_picker/file_picker.dart';
 // ========================================================
 // GLOBAL CONFIG
 // ========================================================
-const String kBackendUrl = "https://gravityai-backend.onrender.com"; 
+const String kBackendUrl = "https://gravityai-backend.onrender.com/"; 
 const String kEarthImg = "assets/images/background.png";
 
 void main() { runApp(const GravityApp()); }
